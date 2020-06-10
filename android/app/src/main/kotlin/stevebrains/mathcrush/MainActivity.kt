1ad836f1be58b87264526d5beeb0bc7697a9f746
Sreenivas K <ksreenivascse@gmail.com>
2020-06-10 11:10:43 +0530

    This is the trail application with the new Firebase projet which as no data

---
+++ android/app/src/main/kotlin/stevebrains/mathcrush/MainActivity.kt
@@ -0,0 +1,6 @@
+package stevebrains.mathcrush
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/resources/api_provider.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import '../models/category.dart';
+import '../models/question.dart';
+
+// var db = new Mysql();
+String _level;
+Future<List<Question>> getQuestions(Category category) async {
+  List<Question> list = List();
+  List<Question> list2 = List();
+  _level = category.level;
+  try {
+    final levelReference = Firestore.instance.collection("AllLevels");
+    QuerySnapshot documentSnapshot = await levelReference
+        .document("M5xgqSw5RA2VaBkQEP5N")
+        .collection("Level-" + _level)
+        .getDocuments();
+    documentSnapshot.documents.forEach((e) {
+      Question levelP = Question.fromMap(e.data);
+      list.add(levelP);
+    });
+  } catch (e) {
+    print("ho");
+  }
+  for (var i = 1; i <= 1; i++) {
+    list2.add(list[i]);
+  }
+  list2.shuffle();
+  print("list $list");
+  print(list2.length);
+  return list;
+}
